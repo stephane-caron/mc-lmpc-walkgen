c37cdf8c76e154ad2af5eac40ef2d4278b40c7bf
Stephane Caron <stephane.caron@normalesup.org>
2016-08-13 17:07:45 +0200

    removed unused plots

--- mpc3d/com_buffer.py
+++ mpc3d/com_buffer.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License along with
 # 3d-mpc. If not, see <http://www.gnu.org/licenses/>.
 
-import pylab
 import time
 
 from numpy import zeros
@@ -103,75 +102,3 @@ class COMAccelBuffer(object):
                 self.comdd_traj.append(comdd)
                 callback(comdd)
                 time.sleep(dT - time.time() + t0)
-
-    def plot_com_traj(self):
-        pylab.ion()
-        pylab.clf()
-        x_traj, y_traj, z_traj = zip(*self.com_traj)
-        pylab.plot(x_traj, color='r')
-        pylab.plot(y_traj, color='g')
-        pylab.plot(z_traj, color='b')
-        pylab.legend(('$\ddot{x}_G$', '$\ddot{y}_G$', '$\ddot{z}_G$'))
-
-    def plot_comdd_traj(self):
-        pylab.ion()
-        pylab.clf()
-        pylab.plot(self.comdd_traj)
-
-    def plot_last_profiles(self, com, comd):
-        """
-        Plot preview positions.
-
-        com -- current COM position
-        comd -- current COM velocity
-        """
-        x_list, xd_list, xdd_list = [], [], []
-        y_list, yd_list, ydd_list = [], [], []
-        z_list, zd_list, zdd_list = [], [], []
-        nb_steps = len(self.comdd_vector) / 3
-        for i in xrange(nb_steps):
-            comdd = self.comdd_vector[3 * i:3 * (i + 1)]
-            # com_list.append(com)
-            x_list.append(com[0])
-            y_list.append(com[1])
-            z_list.append(com[2])
-            # comd_list.append(comd)
-            xd_list.append(comd[0])
-            yd_list.append(comd[1])
-            zd_list.append(comd[2])
-            # comdd_list.append(comdd)
-            xdd_list.append(comdd[0])
-            ydd_list.append(comdd[1])
-            zdd_list.append(comdd[2])
-            com += comd * self.timestep + .5 * comdd * self.timestep ** 2
-            comd += comdd * self.timestep
-
-        pylab.ion()
-        pylab.clf()
-        pylab.subplot(331)
-        pylab.plot(xdd_list, 'r-', label='$\ddot{x}_G$')
-        pylab.legend()
-        pylab.subplot(332)
-        pylab.plot(ydd_list, 'g-', label='$\ddot{y}_G$')
-        pylab.legend()
-        pylab.subplot(333)
-        pylab.plot(zdd_list, 'b-', label='$\ddot{z}_G$')
-        pylab.legend()
-        pylab.subplot(334)
-        pylab.plot(xd_list, 'r-', label='$\dot{x}_G$')
-        pylab.legend()
-        pylab.subplot(335)
-        pylab.plot(yd_list, 'g-', label='$\dot{y}_G$')
-        pylab.legend()
-        pylab.subplot(336)
-        pylab.plot(zd_list, 'b-', label='$\dot{z}_G$')
-        pylab.legend()
-        pylab.subplot(337)
-        pylab.plot(x_list, 'r-', label='$x_G$')
-        pylab.legend()
-        pylab.subplot(338)
-        pylab.plot(y_list, 'g-', label='$y_G$')
-        pylab.legend()
-        pylab.subplot(339)
-        pylab.plot(z_list, 'b-', label='$z_G$')
-        pylab.legend()
